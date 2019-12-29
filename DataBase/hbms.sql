6fbcb68495961ea9a17c46edd5e973abd994a977
akash hadal <akash.hadal@gmail.com>
2019-12-29 23:58:18 +0530

    PLP

--- DataBase/hbms.sql
+++ DataBase/hbms.sql
@@ -33,25 +33,29 @@ CREATE TABLE `booking_info` (
   PRIMARY KEY (`booking_id`,`room_id`,`checkout_date`),
   KEY `room_id` (`room_id`),
   KEY `user_id` (`user_id`),
-  KEY `hotel_id` (`hotel_id`),
-  CONSTRAINT `booking_info_ibfk_1` FOREIGN KEY (`room_id`) REFERENCES `room` (`room_id`),
-  CONSTRAINT `booking_info_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
-  CONSTRAINT `booking_info_ibfk_3` FOREIGN KEY (`hotel_id`) REFERENCES `hotel` (`hotel_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8;
+  KEY `hotel_id` (`hotel_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=58 DEFAULT CHARSET=utf8;
 
 /*Data for the table `booking_info` */
+
+insert  into `booking_info`(`booking_id`,`room_id`,`user_id`,`amount`,`payment_status`,`mode_of_payment`,`checkin_date`,`checkout_date`,`hotel_id`) values 
+(34,3,1,10000.000,'UnPaid','cash','2019-12-30','2019-12-31',1),
+(35,16,2,6542.000,'Done','card','2019-12-29','2019-12-30',12),
+(53,16,55,6542.000,'UnPaid','cash','2019-12-31','2020-01-01',12),
+(54,16,55,6542.000,'UnPaid','cash','2019-12-31','2019-12-31',12),
+(57,12,41,37500.000,'UnPaid','cash','2019-12-30','2020-01-02',11);
 
 /*Table structure for table `food_order` */
 
 DROP TABLE IF EXISTS `food_order`;
 
 CREATE TABLE `food_order` (
-  `order_id` int(10) NOT NULL,
-  `food_id` int(10) NOT NULL,
-  `food_type` varchar(50) NOT NULL,
-  `food_name` varchar(50) NOT NULL,
-  `food_price` double(6,2) NOT NULL,
-  `food_quantity` int(10) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `food_id` int(11) DEFAULT NULL,
+  `food_name` varchar(255) DEFAULT NULL,
+  `food_price` double DEFAULT NULL,
+  `food_quantity` int(11) DEFAULT NULL,
+  `food_type` varchar(255) DEFAULT NULL,
   `room_id` int(11) DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
   `user_name` varchar(255) DEFAULT NULL,
@@ -71,7 +75,7 @@ CREATE TABLE `hibernate_sequence` (
 /*Data for the table `hibernate_sequence` */
 
 insert  into `hibernate_sequence`(`next_val`) values 
-(56);
+(61);
 
 /*Table structure for table `hotel` */
 
@@ -86,39 +90,25 @@ CREATE TABLE `hotel` (
   `image` varchar(1000) DEFAULT NULL,
   PRIMARY KEY (`hotel_id`),
   UNIQUE KEY `hotel_name` (`hotel_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
 
 /*Data for the table `hotel` */
 
 insert  into `hotel`(`hotel_id`,`hotel_name`,`location`,`available_ac`,`available_non_ac`,`image`) values 
-(1,'Taj Hotel','Mumbai',50,66,'https://cdn.pixabay.com/photo/2018/08/08/13/34/taj-mahal-palace-hotel-3592184__340.jpg'),
-(2,'Oberoi','Goa',65,67,'https://cdn.pixabay.com/photo/2015/09/28/21/32/the-palm-962785__340.jpg'),
+(1,'Taj Hotel','Mumbai',50,64,'https://cdn.pixabay.com/photo/2018/08/08/13/34/taj-mahal-palace-hotel-3592184__340.jpg'),
+(2,'Oberoi','Goa',65,65,'https://cdn.pixabay.com/photo/2015/09/28/21/32/the-palm-962785__340.jpg'),
 (3,'Velocity Inc.','Goa',23,30,'https://cdn.pixabay.com/photo/2016/11/17/09/28/hotel-1831072__340.jpg'),
-(4,'Trilok','Banglore',24,53,'https://cdn.pixabay.com/photo/2014/07/05/08/21/pool-384573__340.jpg'),
-(8,'Paradise Resort','Goa',45,55,'https://cdn.pixabay.com/photo/2019/12/13/06/58/stockholm-4692351__340.jpg'),
-(11,'Mansion','Mumbai',10,20,'https://cdn.pixabay.com/photo/2015/03/26/10/10/building-691033__340.jpg'),
-(12,'Hotel Treeside','Pune',21,40,'https://cdn.pixabay.com/photo/2018/01/03/04/14/architecture-3057553__340.jpg'),
-(21,'Cosmopolitan Hotel','Delhi',100,150,'https://cdn.pixabay.com/photo/2017/07/01/09/08/cosmopolitan-hotel-2460949__340.jpg');
+(4,'Trilok','Banglore',24,49,'https://cdn.pixabay.com/photo/2014/07/05/08/21/pool-384573__340.jpg'),
+(8,'Paradise Resort','Goa',44,56,'https://cdn.pixabay.com/photo/2019/12/13/06/58/stockholm-4692351__340.jpg'),
+(11,'Mansion','Mumbai',9,17,'https://cdn.pixabay.com/photo/2015/03/26/10/10/building-691033__340.jpg'),
+(12,'Hotel Treeside','Pune',18,40,'https://cdn.pixabay.com/photo/2018/01/03/04/14/architecture-3057553__340.jpg'),
+(21,'Cosmopolitan Hotel','Delhi',78,90,'https://cdn.pixabay.com/photo/2017/07/01/09/08/cosmopolitan-hotel-2460949__340.jpg');
 
 /*Table structure for table `menu_card` */
 
 DROP TABLE IF EXISTS `menu_card`;
 
 CREATE TABLE `menu_card` (
-  `food_id` int(10) NOT NULL,
-  `food_type` varchar(50) NOT NULL,
-  `food_name` varchar(50) NOT NULL,
-  `food_price` double(6,2) DEFAULT NULL,
-  PRIMARY KEY (`food_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/*Data for the table `menu_card` */
-
-/*Table structure for table `menu_cart` */
-
-DROP TABLE IF EXISTS `menu_cart`;
-
-CREATE TABLE `menu_cart` (
   `food_id` int(11) NOT NULL,
   `food_name` varchar(255) DEFAULT NULL,
   `food_price` double DEFAULT NULL,
@@ -126,7 +116,7 @@ CREATE TABLE `menu_cart` (
   PRIMARY KEY (`food_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*Data for the table `menu_cart` */
+/*Data for the table `menu_card` */
 
 /*Table structure for table `room` */
 
@@ -142,8 +132,7 @@ CREATE TABLE `room` (
   `room_type` varchar(10) NOT NULL,
   `image` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`room_id`,`room_capacity`),
-  KEY `hotel_id` (`hotel_id`),
-  CONSTRAINT `room_ibfk_1` FOREIGN KEY (`hotel_id`) REFERENCES `hotel` (`hotel_id`)
+  KEY `hotel_id` (`hotel_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
 
 /*Data for the table `room` */
@@ -159,11 +148,11 @@ insert  into `room`(`room_id`,`room_rent`,`room_facility`,`room_capacity`,`room_
 (9,800.000,'NON-AC',3,'Available',4,'2','https://cdn.pixabay.com/photo/2014/09/26/19/45/bedroom-462772__340.jpg'),
 (10,4000.000,'AC',4,'Available',8,'3','https://cdn.pixabay.com/photo/2014/09/26/19/45/bedroom-462772__340.jpg'),
 (11,2500.000,'NON-AC',3,'Available',8,'3','https://cdn.pixabay.com/photo/2016/06/03/18/36/bedroom-window-1434067__340.jpg'),
-(12,12500.000,'AC',12,'Available',11,'8','https://cdn.pixabay.com/photo/2018/02/24/17/17/window-3178666__340.jpg'),
-(13,8000.000,'NON-AC',7,'Available',11,'5','https://cdn.pixabay.com/photo/2018/02/24/17/17/window-3178666__340.jpg'),
+(12,12500.000,'AC',8,'Available',11,'8','https://cdn.pixabay.com/photo/2018/02/24/17/17/window-3178666__340.jpg'),
+(13,8000.000,'NON-AC',6,'Available',11,'5','https://cdn.pixabay.com/photo/2018/02/24/17/17/window-3178666__340.jpg'),
 (14,2400.000,'AC',2,'Available',2,'2','https://cdn.pixabay.com/photo/2016/03/16/22/17/hotel-room-1261900__340.jpg'),
-(15,7410.000,'AC',0,'Available',11,'2','https://cdn.pixabay.com/photo/2016/04/15/11/48/hotel-1330847__340.jpg'),
-(16,6542.000,'AC',0,'Available',12,'2','https://cdn.pixabay.com/photo/2016/07/08/23/33/palmetto-bay-1505455__340.jpg');
+(15,7410.000,'AC',2,'Available',11,'2','https://cdn.pixabay.com/photo/2016/04/15/11/48/hotel-1330847__340.jpg'),
+(16,6542.000,'AC',2,'Available',12,'2','https://cdn.pixabay.com/photo/2016/07/08/23/33/palmetto-bay-1505455__340.jpg');
 
 /*Table structure for table `user` */
 
@@ -178,43 +167,25 @@ CREATE TABLE `user` (
   `nationality` varchar(50) DEFAULT NULL,
   `phoneNumber` varchar(50) DEFAULT NULL,
   `address` varchar(255) DEFAULT NULL,
+  `hotelId` int(255) DEFAULT NULL,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `unique` (`user_email`),
   KEY `password` (`password`)
-) ENGINE=InnoDB AUTO_INCREMENT=56 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=61 DEFAULT CHARSET=utf8;
 
 /*Data for the table `user` */
 
-insert  into `user`(`user_id`,`user_name`,`user_email`,`user_type`,`password`,`nationality`,`phoneNumber`,`address`) values 
-(1,'akash','akash@gmail.com','admin','Akash@11','Indian','7066705460','Boisar'),
-(2,'pooja','pooja@gmail.com','user','Pooja@123','Indian','7066705460','Nagpur'),
-(3,'diksha','diksha@gmail.com','employee','diksha@123','Indian','7066705460','Boisar'),
-(40,'admin','admin@gmail.com','admin','Admin@123','Indian','7066705460','Boisar'),
-(41,'user','user@gmail.com','user','User@123','Indian','7066705460','Boisar'),
-(42,'manager','manager@gmail.com','manager','Manager@123','Indian','7066705460','Boisar'),
-(43,'employee','employee@gmail.com','employee','Employee@123','Indian','7066705460','Boisar'),
-(52,'Ramesh','ramesh@gmail.com','user','Ramesh@123','Indian','9423372928','House No. 135 C,Tambada Pada,Near R'),
-(53,'Sheetal','sheetal@gmail.com','user','Sheetal@123','Indian','9512233366','Pune'),
-(55,'Nishi Shah','nishi@gmail.com','manager','Nishi@123','Indian','9754220236','Dombivali');
-
-/*Table structure for table `user_other_info` */
-
-DROP TABLE IF EXISTS `user_other_info`;
-
-CREATE TABLE `user_other_info` (
-  `id` int(11) NOT NULL,
-  `address` varchar(255) DEFAULT NULL,
-  `nationality` varchar(255) DEFAULT NULL,
-  `password` varchar(10) DEFAULT NULL,
-  `phone_no` bigint(20) DEFAULT NULL,
-  `user_email` varchar(255) DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `user_name` varchar(255) DEFAULT NULL,
-  `user_type` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/*Data for the table `user_other_info` */
+insert  into `user`(`user_id`,`user_name`,`user_email`,`user_type`,`password`,`nationality`,`phoneNumber`,`address`,`hotelId`) values 
+(1,'akash','akash@gmail.com','admin','Akash@11','Indian','7066705460','Boisar',99999),
+(2,'pooja','pooja@gmail.com','user','Pooja@123','indian','7066705460','Pune',88888),
+(40,'admin','admin@gmail.com','admin','Admin@123','Indian','7066705460','Boisar',99999),
+(41,'user','user@gmail.com','user','User@123','Indian','7066705460','Boisar',88888),
+(42,'manager','manager@gmail.com','manager','Manager@123','Indian','7066705460','Boisar',1),
+(43,'employee','employee@gmail.com','employee','Employee@123','Indian','7066705460','Boisar',2),
+(52,'Ramesh','ramesh@gmail.com','user','Ramesh@123','Indian','9423372928','House No. 135 C,Tambada Pada,Near R',88888),
+(53,'Sheetal','sheetal@gmail.com','user','Sheetal@123','Indian','9512233366','Pune',88888),
+(59,'surve','surve@gmail.com','user','Surve@123','indian','7541023658','House No. 135 C,Tambada Pada,Near R',88888),
+(60,'simran','simran@gmail.com','manager','Simran@123','pakistani','7066754555','House No. 135 C,Tambada Pada,Near R',0);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
